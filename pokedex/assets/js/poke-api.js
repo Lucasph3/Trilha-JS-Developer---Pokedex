0b96fe84d445db4e8e05a159958aa1d429817f24
Lucasph3 <119544801+Lucasph3@users.noreply.github.com>
2023-12-18 22:15:54 -0400

    Add files via upload

---
+++ pokedex/assets/js/poke-api.js
@@ -0,0 +1,40 @@
+
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+    // tipo
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    pokemon.types = types
+    pokemon.type = type
+    // imagem
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    // url de detalhes
+    pokemon.urlDetail = pokeDetail.abilities[0].ability.url
+    // experiencia
+    pokemon.experience = pokeDetail.base_experience
+    // habilidades
+    pokemon.abilities = pokeDetail.abilities.map((ability) => ability.ability.name)
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}
